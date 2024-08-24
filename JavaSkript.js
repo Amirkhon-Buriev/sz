a836bb238eecf6f2d89c05092ed8137737f2826d
Amirkhon-Buriev <amirkhonburiyev@gmail.com>
2024-08-24 10:56:33 +0500

    Add files via upload

---
+++ JavaSkript.js
@@ -0,0 +1,21 @@
+// let a = 3
+
+// let b  = 6
+
+// console.log(a + b);
+
+
+
+// let a = Number('12');87лг8ею6пауфы
+
+
+// console.log(typeof a);
+
+
+
+//let random = Math.floor(Math.random()*100)
+
+//if(random % 2 == 0){
+  //  console.log(w);
+    
+//}
